106f4213874aa1c8b632f09c009bdb5bd16587fd
Vivek Vishwanath <vivek@airblackbox.com>
2014-08-22 11:08:52 +0100

    Sendgrid update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,8 +32,8 @@ Publify::Application.configure do
   :address        => 'smtp.sendgrid.net',
   :port           => '587',
   :authentication => :plain,
-  :user_name      => ENV['SENDGRID_USERNAME'],
-  :password       => ENV['SENDGRID_PASSWORD'],
+  :user_name      => 'app28767688@heroku.com',
+  :password       => 'qrqg99jy',
   :domain         => 'heroku.com',
   :enable_starttls_auto => true
 }
